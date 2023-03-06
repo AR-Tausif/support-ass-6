00727d69eb52342d9ff1ecc9b2620e53a194ae5d
Tausif Ahmed <120933222+AR-Tausif@users.noreply.github.com>
2023-03-06 20:41:18 +0600

    last submit

--- js/script.js
+++ js/script.js
@@ -1,40 +1,53 @@
-const dataLoad = async (dataLimit)=>{
-    const url = 'https://openapi.programming-hero.com/api/ai/tools';
-    const res = await fetch(url);
-    const data = await res.json();
-    displayData(data, dataLimit)
+
+const dataLoad = async (dataLimit) => {
+  const url = 'https://openapi.programming-hero.com/api/ai/tools'
+  const res = await fetch(url)
+  const data = await res.json()
+  displayData(data.data.tools, dataLimit)
 }
+const sortByDate = () =>{
+  const url = 'https://openapi.programming-hero.com/api/ai/tools'
 
-const displayData = (data, dataLimit) =>{
-    const cardContainer = document.getElementById('card-container');
-    console.log(data);
-    let toolsInfo = data.data.tools;
-    // functionality added for load more button
-    const showAll = document.getElementById("show-all");
-      if(dataLimit && toolsInfo.length > 10){
-          toolsInfo = toolsInfo.slice(0, 6);
-  
-          showAll.classList.remove('d-none')
-      }else{
-          showAll.classList.add('d-none')
-      }
+  fetch(url)
+  .then(res => res.json())
+  .then(data => {
+    data.data.tools.sort((a,b) => new Date(a.published_in) - new Date(b.published_in));
 
-    // Display data with foreach loop
-    toolsInfo.forEach(tool => {
-        const dataFeature = tool.features;
-        const newDiv = document.createElement('div');
-        newDiv.classList.add('col');
-        newDiv.innerHTML = `
+    displayData(data?.data?.tools)
+    document.getElementById('show-all').classList.add('d-none')
+
+  })
+}
+ 
+const displayData = (data, dataLimit) => {
+  const cardContainer = document.getElementById('card-container');
+  cardContainer.innerText = '';
+  // console.log(data)
+  let toolsInfo = data;
+  // functionality added for load more button
+  const showAll = document.getElementById('show-all')
+  if (dataLimit && toolsInfo.length > 10) {
+    toolsInfo = toolsInfo.slice(0, 6)
+ 
+    showAll.classList.remove('d-none')
+  } else {
+    showAll.classList.add('d-none')
+  }
+ 
+  // Display data with foreach loop
+  toolsInfo.forEach((tool) => {
+    const dataFeature = tool.features
+    const newDiv = document.createElement('div')
+    newDiv.classList.add('col')
+    newDiv.innerHTML = `
             <div class="card h-100">
                 <img src="${tool.image}" class="card-img-top" alt="...">
                 <div class="card-body">
-                  <h5 class="card-title">${tool.name}</h5>
+                  <h5 class="card-title">Features</h5>
                   <ol class="card-text">
-                    ${dataFeature.map((item) => {
-                        return `<li>${item}</li>`;
-                    })}
+                    ${dataFeature.map(a=> (`<li>${a}</li>`)).join("")}
                   </ol>
-
+ 
                 </div>
                 <hr class="hr-mix mx-auto">
                 <div class="d-flex justify-content-between align-items-center px-4">
@@ -45,39 +58,90 @@ const displayData = (data, dataLimit) =>{
                         <span class="fw-bold px-2">${tool.published_in}</span>
                         </div>
                       </div>
-
-                       <button onclick="dataModalShow(${tool})">
+ 
+                       <button class="cta-button border-danger  rounded-pill" >
                        <i class="fa-sharp fa-solid fa-arrow-right bg-light text-danger p-2 rounded rounded-circle bgmuted" data-bs-toggle="modal" data-bs-target="#exampleModal"></i></button>
-
+ 
                 </div>
             </div>
         `
-
-
-        cardContainer.appendChild(newDiv);
-    });
-    loadingSpinner(false)
+ 
+    const btn = newDiv.querySelector('.cta-button')
+    btn.onclick = () => dataModalShow(tool.id, tool.features)
+ 
+    cardContainer.appendChild(newDiv)
+  })
+  loadingSpinner(false)
 }
-
-document.getElementById('show-more-btn').addEventListener('click', function(){
-    dataLoad();
-    loadingSpinner(true)
+ 
+document.getElementById('show-more-btn').addEventListener('click', function () {
+  dataLoad()
+  loadingSpinner(true)
 })
-
-
-const loadingSpinner  = isLoad =>{
-const spinnerSection = document.getElementById("spinner");
-if(isLoad){
+ 
+const loadingSpinner = (isLoad) => {
+  const spinnerSection = document.getElementById('spinner')
+  if (isLoad) {
     spinnerSection.classList.remove('d-none')
-}else{
+  } else {
     spinnerSection.classList.add('d-none')
+  }
 }
+const dataIdFetch = async (id) =>{
+  const idUrl = `https://openapi.programming-hero.com/api/ai/tool/${id}`
+  const res = await fetch(idUrl);
+  const data = await res.json();
+  dataModalShowDetails(data)
+};
+ 
+const dataModalShow = (data, feature) => {
+  dataIdFetch(data)
+  //   // Modal Features
+const modalFeatueDetails = document.getElementById('modal-feature-details');
+modalFeatueDetails.innerHTML = `
+  ${feature.map(a=> (`<li>${a}</li>`)).join("")}
+`;  
 }
 
-const dataModalShow = data =>{
-  // const modalTitle = document.getElementById('exampleModalLabel');
-  // modalTitle.textContent = data.name;
-  console.log(data);
-}
+  const dataModalShowDetails = details =>{
+    // Modal Title
+  const modalTitle = document.getElementById('modal-body-desc');
+  modalTitle.innerHTML = `
+  <p>${details.data.description}</p>
+  `;
 
+  // Modal Pricing
+  const modalPriceBasic = document.getElementById('price-basic')
+  modalPriceBasic.innerHTML = `
+            <p>${details.data.pricing[0].price} </br> ${details.data.pricing[0].plan}</p>
+  `;
+  const modalPricePro = document.getElementById('price-pro');
+  modalPricePro.innerHTML = `
+  <p>${details.data.pricing[1].price ? details.data.pricing[1].price : 'Free Cost'} </br> ${details.data.pricing[1].plan ? details.data.pricing[1].plan : 'Free Cost'}</p>
+`;
+  const modalPriceEnterprise = document.getElementById('price-enterprise')
+  modalPriceEnterprise.innerHTML = `
+  <p>${details.data.pricing[2].price} </br> ${details.data.pricing[2].plan}</p>
+`;
+
+  // Modal Integretions
+  const integrations = details.data.integrations
+  const modalIntegrations = document.getElementById('integrations');
+  modalIntegrations.innerHTML = `
+  ${integrations.map(a=> (`<li>${a?a:'no data'}</li>`)).join("")}
+`;
+
+    // MODAL IMAGE SECTION ADD HERE]
+    const modalImageAccuracy = document.getElementById('modal-accuracy');
+    modalImageAccuracy.innerText = `${details.data.accuracy.score * 100 ===0 ?  "" :details.data.accuracy.score * 100 + "% Accuracy" }`
+    const modalDetailsImage = document.getElementById('modal-detail-image');
+    modalDetailsImage.setAttribute('src', `${details.data.image_link[0]}`)
+console.log(details.data.input_output_examples[0].input);
+    const modalImageHeading = document.getElementById('modal-image-title');
+    modalImageHeading.innerText = `${details.data.input_output_examples[0].input}`
+    const modalImageDesc = document.getElementById('modal-image-desc');
+    modalImageDesc.innerText = `${details.data.input_output_examples[0].output}`
+    console.log(details.data);
+  }
+ 
 dataLoad(6)
